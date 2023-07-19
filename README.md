fba1ac507837e4abe5008b8ad9b4cecfa750941b
Lemi0002 <51885072+Lemi0002@users.noreply.github.com>
2023-07-19 14:49:40 +0200

    Edit package script
    * Edit package script functionality
    * Add more information to README

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
-# Arch-Linux-Configuration
-This repository contains scripts and information on how to install Arch Linux.
-As a general guide the [Arch Linux Wiki](https://archlinux.org) shall be consulted.
+# Arch Linux configuration
+This repository contains scripts and information on how to configure Arch Linux.
+As a general guide the [Arch Linux wiki](https://archlinux.org) shall be consulted.
 
 # Preparations
 Before any of these scripts can be run, follow the [installation guide](https://archlinux.org/title/installation_guide) and make sure the `git` package is installed.
-Furthermore, a local user account should be set up according to [installation guide](https://archlinux.org/title/General_recommendations).
+Furthermore, a local user account should be set up according to [general recommendations](https://archlinux.org/title/General_recommendations).
 
 # Bluetooth
 If the host has Bluetooth capabilities the provided packages can be installed.
-See [installation guide](https://archlinux.org/title/Bluetooth) for more information.
-For some reason it might not be possible to connect a Bluetooth headphone.
-In that case follow [installation guide](https://archlinux.org/title/bluetooth_headset).
+See [bluetooth](https://archlinux.org/title/Bluetooth) for more information.
+For some reason it might not be possible to connect a Bluetooth headset.
+In that case follow [bluetooth headset](https://archlinux.org/title/bluetooth_headset).
+
+# Wifi
+By using `wpa_supplicant` one might experience throttled download and upload speeds.
+This might be fixed by using `iwd` instead.
+In that case follow [iwd](https://wiki.archlinux.org/title/iwd).
--- scripts/packages.py
+++ scripts/packages.py
@@ -1,15 +1,25 @@
+'''
+Script used for installing all essential packages and enabling certain services.
+'''
+
 import subprocess
 
 command = ['sudo', 'pacman', '-Su']
 packages = [
+    'alacritty',
     'awesome',
     'cmake',
+    'git',
+    'github-cli',
     'gnome',
     'ninja',
+    'neovim',
+    'networkmanager',
     'npm',
     'python-pip',
     'ripgrep',
     'rust',
+    'sddm',
     'unzip',
     'wget',
     'wl-clipboard',
@@ -22,6 +32,9 @@ packages_bluetooth = [
     'bluez-utils',
     'pulseaudio-alsa',
     'pulseaudio-bluetooth',
+]
+packages_wifi = [
+    'iwd',
 ]
 
 
@@ -42,7 +55,24 @@ if select('Install bluetooth packages?'):
     log('Installing bluetooth packages')
     subprocess.run([*command, *packages_bluetooth])
 
-if select('Enable bluetooth.service deamon?'):
-    log('Enabling bluetooth.service deamon')
+if select('Install wifi packages?'):
+    log('Installing wifi packages')
+    subprocess.run([*command, *packages_wifi])
+
+if select('Install yay?'):
+    log('Installing yay')
+    subprocess.run(['git', 'clone', 'https://aur.archlinux.org/yay.git'])
+    subprocess.run(['makepkg', '-si'], cwd='yay')
+    subprocess.run(['rm', '-drf', 'yay'])
+
+if select('Bluetooth: Enable bluetooth.service daemon?'):
+    log('Bluetooth: Enabling bluetooth.service daemon')
     subprocess.run(['sudo', 'systemctl', '--now',
                    'enable', 'bluetooth.service'])
+
+if select('Wifi: Disable wpa_supplicant.service and enable iwd.service daemon?'):
+    log('Wifi: Disabling wpa_supplicant.service daemon')
+    subprocess.run(['sudo', 'systemctl', '--now',
+                   'disable', 'wpa_supplicant.service'])
+    log('Wifi: Enabling iwd.service daemon')
+    subprocess.run(['sudo', 'systemctl', '--now', 'enable', 'iwd.service'])
